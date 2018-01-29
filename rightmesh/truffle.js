55d9a9c6240da6a1840a1495c93f6b9da0b4d1fe
Jude Zhu <judezhu@Judes-MacBook-Pro.local>
2018-01-29 07:12:53 -0800

    Deployment with geth and add documentation.

--- rightmesh/truffle.js
+++ rightmesh/truffle.js
@@ -4,8 +4,15 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 3, // Ropsten network id
+      from: "0xa30109d16db929bb45e08db7127d92a5f5114472", //account for deoployment
+      gas: 4000000
+    }   
   }
 };
