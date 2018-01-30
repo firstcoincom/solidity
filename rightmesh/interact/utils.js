4e938dfb32c0073cc2194c1592e5e537f047056e
Kartik <kartik@firstcoin.com>
2018-01-30 11:10:38 -0800

    restructuing the code to allow config for scrips

---
+++ rightmesh/interact/utils.js
@@ -0,0 +1,45 @@
+const Web3 = require('web3');
+const fs = require("fs");
+const config = require('./config');
+
+const getWeb3 = rpcHost => new Web3(new Web3.providers.HttpProvider(rpcHost));
+const web3 = getWeb3(config.rpcHost);
+
+const convertEthToWei = n => new web3.BigNumber(web3.toWei(n, 'ether'));
+
+const latestTime = () => {
+  // truffle version 4.0.x still depends on web3 version 0.2x.x
+  // please refer to https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethgetblock
+  // for web 3 documentation
+  return web3.eth.getBlock('latest').timestamp;
+}
+
+const duration = {
+  seconds: val => val,
+  minutes: val => val * duration.seconds(60),
+  hours: val => val * duration.minutes(60),
+  days: val => val * duration.hours(24),
+  weeks: val => val * duration.days(7),
+  years: val => val * duration.days(365),
+};
+
+const getJson = file => {
+  const contents = fs.readFileSync(file);
+  return JSON.parse(contents);
+}
+
+const crowdsaleJsonFile = 'build/contracts/MeshCrowdsale.json';
+
+const getCrowdsaleContract = (_web3, contractAddress) => {
+  const jsonContent = getJson(crowdsaleJsonFile);
+  const Crowdsale = _web3.eth.contract(jsonContent.abi);
+  return Crowdsale.at(crowdsaleAddress);
+}
+
+module.exports = {
+  convertEthToWei,
+  latestTime,
+  duration,
+  getWeb3,
+  getCrowdsaleContract,
+}
--- rightmesh/interact/whitelist.js
+++ rightmesh/interact/whitelist.js
@@ -1,25 +1,21 @@
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var fs = require("fs");
+const config = require('./config');
+const whitelistConfig = require('./whitelist-config');
+const utils = require('./utils');
 
-// Crowdsale Contract Properties
-const contractJSON = "build/contracts/MeshCrowdsale.json";
-const contractAddress = "0x0E32C1A5C1F8873f67eAE883fF8D228A713D5C72";
-const rate = 100;
-const ownerAccount = "0xa30109d16db929bb45e08db7127d92a5f5114472";
-
-// Contributor Properties
-const contributorAccount = "0x4858e6E0991C3eb852D0e3c10E9Ce1ed4aB88BFc";
-const limitByEth = 2;
-
-// Get ABI interface from json file
-var contents = fs.readFileSync(contractJSON);
-var jsonContent = JSON.parse(contents);
-var MeshCrowdsaleContract = web3.eth.contract(jsonContent.abi);
+const web3 = utils.getWeb3(config.rpcHost);
 
 // instantiate by address
-var contractInstance = MeshCrowdsaleContract.at(contractAddress);
+var crowdsaleInstance = utils.getCrowdsaleContract(
+  web3,
+  config.crowdsaleAddress
+);
 
-// white list a given account 
-contractInstance.setLimit(contributorAccount, web3.toWei( rate * limitByEth, 'ether'), {gas: 92000, from: ownerAccount});
-
+// white list a given accounts
+crowdsaleInstance.setLimit(
+  whitelistConfig.accountsToWhitelist,
+  web3.toWei( whitelistConfig.limitByEth, 'ether'),
+  {
+    gas: whitelistConfig.gas,
+    from: config.ownerAccount
+  }
+);
