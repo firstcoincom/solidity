97d524d34d418dd311c5ff74f462eb8b38b21f10
Kartik <kartik@firstcoin.com>
2018-01-30 16:09:46 -0800

    adding pause / unpause script

---
+++ rightmesh/scripts/unpause_token.js
@@ -0,0 +1,19 @@
+const generalSettings = require('../config/general-settings');
+const tokenConfig = require('../config/token-config');
+const utils = require('../utils/utils');
+
+const web3 = utils.getWeb3(generalSettings.rpcHost);
+
+// Instantiate token by token contract address
+var TokenInstance = utils.getTokenContract(
+  web3,
+  tokenConfig.tokenAddress
+);
+
+// Call unpause on the token contract
+TokenInstance.unpause(
+  {
+    gas: tokenConfig.gas,
+    from: tokenConfig.ownerAccount
+  }
+);
