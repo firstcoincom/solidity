2f6cdf2326de62ac4e053a750f4c8fb7a3d25ae3
Kartik <kartik@firstcoin.com>
2018-01-31 09:10:11 -0800

    adding mint tokens script

---
+++ rightmesh/config/minting-config.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    address: '0x5D51E3558757Bfdfc527867d046260fD5137Fc0F',
+    tokenAmountETH: 100,
+  },
+  {
+    address: '0x5D51E3558757Bfdfc527867d046260fD5137Fc0F',
+    tokenAmountETH: 100,
+  },
+  {
+    address: '0x5D51E3558757Bfdfc527867d046260fD5137Fc0F',
+    tokenAmountETH: 100,
+  },
+  {
+    address: '0x5D51E3558757Bfdfc527867d046260fD5137Fc0F',
+    tokenAmountETH: 100,
+  },
+  {
+    address: '0x5D51E3558757Bfdfc527867d046260fD5137Fc0F',
+    tokenAmountETH: 100,
+  },
+]
