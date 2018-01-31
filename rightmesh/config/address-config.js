2f262ef00d4d8a7dacb226418ee35a2e2c8357d0
Kartik <kartik@firstcoin.com>
2018-01-31 09:25:11 -0800

    adding whitelisting agent script

--- rightmesh/config/address-config.js
+++ rightmesh/config/address-config.js
@@ -3,5 +3,6 @@ module.exports = {
     // change these variables to interact with the right contract
     crowdsaleAddress: '0x5D51E3558757Bfdfc527867d046260fD5137Fc0F',
     tokenAddress: '0xf3C3b1eD7Fc8F82641cCE6CcaCE792e46B42399F',
+
+    whitelistAgentAddress: '0xa30109d16db929bb45e08db7127d92a5f5114472',
   }
-  
---
+++ rightmesh/scripts/add_whitelisting_agent.js
@@ -0,0 +1,20 @@
+const generalSettings = require('../config/general-settings');
+const crowdsaleConfig = require('../config/crowdsale-config');
+const addressConfig = require('../config/address-config');
+const utils = require('../utils/utils');
+
+const web3 = utils.getWeb3(generalSettings.rpcHost);
+
+// Instantiate crowdsale by crowdsale contract address
+var CrowdsaleInstance = utils.getCrowdsaleContract(
+  web3,
+  addressConfig.crowdsaleAddress
+);
+
+CrowdsaleInstance.setWhitelistingAgent(
+  addressConfig.whitelistAgentAddress,
+  {
+    from: crowdsaleConfig.ownerAccount,
+    gas: crowdsaleConfig.gas,
+  }
+)
