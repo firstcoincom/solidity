1f0b3f7ca7fd7bacf0d65e1a2c8935947275696d
Kartik <kartik@firstcoin.com>
2018-01-31 10:30:36 -0800

    adding script to allow exceptions in transfers

---
+++ rightmesh/scripts/9_add_transfer_exception.js
@@ -0,0 +1,22 @@
+const generalSettings = require('../config/general-settings');
+const addressConfig = require('../config/address-config');
+const gasConfig = require('../config/gas-config');
+const utils = require('../utils/utils');
+
+const web3 = utils.getWeb3(generalSettings.rpcHost);
+
+// Instantiate token by token contract address
+var TokenInstance = utils.getTokenContract(
+  web3,
+  addressConfig.tokenAddress
+);
+
+// Call updateAllowedTransfers on the token contract
+TokenInstance.updateAllowedTransfers(
+  addressConfig.transferExceptionAddress,
+  true,
+  {
+    gas: gasConfig.methodGas,
+    from: addressConfig.ownerAddress,
+  }
+);
